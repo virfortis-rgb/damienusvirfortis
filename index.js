71c8205569e5e9d3e8ff863213cdeb272fc2a1b9
Damien Joubert <damienusvirfortis@gmail.com>
2026-04-26 19:46:35 +0900

    get importing json into js file to work

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
 import data from './projects.json' with { type: 'json' };
+data.forEach(project => {
+  console.log(project.id);
+});
 
 const template = document.querySelector('#projet-card-template');
 const container = document.querySelector('.row');
 
-const clone = template.content.cloneNode(true);
+data.forEach((project) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector('.project-img').src = project.img_path;
+  clone.querySelector('.gh-repo').href = project.links.gh;
+  clone.querySelector('.site').href = project.links.live;
+  clone.querySelector('.title').textContent = project.title;
+  clone.querySelector('.role').textContent = `@${project.role}`;
+  const featuresContainer = clone.querySelector('.roles-features');
+  project.features.forEach((feature) => {
+    const span = document.createElement('span');
+    span.classList.add('badge', 'text-bg-success', 'feature', 'me-1');
+    span.textContent = feature;
+    featuresContainer.appendChild(span);
+  });
+  clone.querySelector('.description').textContent = project.description;
+  clone.querySelector('.stack').textContent = project.stack.join(' | ');
 
-// Modify the cloned content
-clone.querySelector('.project-img').   textContent = "Hello World";
-clone.querySelector('#gh').  textContent = "Hello World";
-clone.querySelector('#site').  textContent = "Hello World";
-clone.querySelector('#title').textContent = "Hello World";
-clone.querySelector('#role').textContent = "@" + "Hello World";
-clone.querySelector('#features').textContent = "Hello World";
-clone.querySelector('#description').textContent = "Hello World";
-clone.querySelector('#stack').   textContent = "Hello World";
-
-container.appendChild(clone);
-
-const projects = {
-  wmt =  {
-    imgPath: "./wmt/Tofu Logo.png",
-    gh: "https://github.com/virfortis-rgb/wheres-my-tofu",
-    site: "https://www.wheresmytofu.me/",
-    title: "Where's My Tofu?",
-    role: "lead developer",
-    features: "",
-    description: "WMT is a shopping list web application that lets you compare the prices of your groceries, so that you can choose the best price for your groceries. WMT allows you to compare prices from different stores in the Osaka, Nishi-ku area.",
-    stack: "Ruby | Ruby on Rails | PostgreSQL | JavaScript | Heroku | HTML | CSS etc."
-  },
-  maiKusuri =  {
-    imgPath: "./default.png",
-    gh: "",
-    site: "",
-    title: "Where's My Tofu?",
-    role: "lead developer",
-    features: "",
-    description: "",
-    stack: ""
-  },
-  maiKusuri =  {
-    imgPath: "./default.png",
-    gh: "",
-    site: "",
-    title: "Where's My Tofu?",
-    role: "lead developer",
-    features: "",
-    description: "",
-    stack: ""
-  }
-};
+  container.appendChild(clone);
+});

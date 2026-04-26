846f84a3d0db3ec2b082728e3a39e700caa47ce9
Damien Joubert <damienusvirfortis@gmail.com>
2026-04-26 20:14:08 +0900

    add pictures and small ui touches

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const container = document.querySelector('.row');
 data.forEach((project) => {
   const clone = template.content.cloneNode(true);
   clone.querySelector('.project-img').src = project.img_path;
-  clone.querySelector('.gh-repo').href = project.links.gh;
+  clone.querySelector('.gh-repo').href = project.links.github;
   clone.querySelector('.site').href = project.links.live;
   clone.querySelector('.title').textContent = project.title;
   clone.querySelector('.role').textContent = `@${project.role}`;
-  const featuresContainer = clone.querySelector('.roles-features');
-  project.features.forEach((feature) => {
-    const span = document.createElement('span');
-    span.classList.add('badge', 'text-bg-success', 'feature', 'me-1');
-    span.textContent = feature;
-    featuresContainer.appendChild(span);
-  });
+  const featuresContainer = clone.querySelector('.features');
+  if (project.features != null) {
+    project.features.forEach((feature) => {
+      const span = document.createElement('span');
+      span.classList.add('badge', 'text-bg-success', 'feature', 'me-1');
+      span.textContent = feature;
+      featuresContainer.appendChild(span);
+    });
+  }
   clone.querySelector('.description').textContent = project.description;
   clone.querySelector('.stack').textContent = project.stack.join(' | ');
-
+  // TODO add icons to stack --- check HTML
   container.appendChild(clone);
 });
